e8405a77b1a99d9666d4ef06ad4ea5616a0a6d40
chaudhuree <67362262+chaudhuree@users.noreply.github.com>
2022-11-21 21:47:59 +0600

    update with more description

--- flexibleLocalSotrage.jsx
+++ flexibleLocalSotrage.jsx
@@ -4,13 +4,16 @@
 
 import * as React from 'react'
 
+
 function useLocalStorageState(
   key,
   defaultValue = '',
   // the = {} fixes the error we would get from destructuring when no argument was passed
-  // Check https://jacobparis.com/blog/destructure-arguments for a detailed explanation
   {serialize = JSON.stringify, deserialize = JSON.parse} = {},
 ) {
+  ///////////////////////////
+  //set the state value start
+  ///////////////////////////
   const [state, setState] = React.useState(() => {
     const valueInLocalStorage = window.localStorage.getItem(key)
     if (valueInLocalStorage) {
@@ -22,19 +25,43 @@ function useLocalStorageState(
         window.localStorage.removeItem(key)
       }
     }
+    //if the if "condition's value" does not fulfill then this return works
     return typeof defaultValue === 'function' ? defaultValue() : defaultValue
   })
+  ////////////////////////////
+  //set the state value finish
+  ////////////////////////////
 
-  const prevKeyRef = React.useRef(key)
+  //⭐⭐ if the key change that means.
+  /*
+  suppose ami atokhon name key te amr value save kortecilam
+  but akhn ami chaiteci j ami genderName key te amr data set korbo
+  then next step
+   */
+  const prevKeyRef = React.useRef(key)  //aita present key mean name jeta age cilo oitare reference korbe apatoto
+  /*
+  useRef re rendr kore na. so aita condition check korar time a ,
+  render hobe na and previouse key and present key different thakbe
+   */
 
-  // Check the example at src/examples/local-state-key-change.js to visualize a key change
+
   React.useEffect(() => {
-    const prevKey = prevKeyRef.current
+    const prevKey = prevKeyRef.current //current a value ta thake
     if (prevKey !== key) {
+      /*
+      now,logic check ==
+      prevKey hocce name and new key hocce genderName(suppose)
+      so bojha jaitice j amra r name a ai value set korbo na
+      so now, amra ager key mane name ta delete kore dibo. so er
+      sob value soho aita delete hoye jabe
+       */
       window.localStorage.removeItem(prevKey)
     }
-    prevKeyRef.current = key
+    prevKeyRef.current = key //then prevKeyRef a new key mane genderName set korlam
     window.localStorage.setItem(key, serialize(state))
+    /*
+    ⬆⬆ ai line a main function mane Greeting theke asha setName er value genderName a save kore dilam
+     */
   }, [key, state, serialize])
 
   return [state, setState]
@@ -63,3 +90,4 @@ function App() {
 }
 
 export default App
+
